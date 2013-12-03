59985e10884e8621a1d3f73ac0823b247ef27b6c
Markus Stenberg <markus.stenberg@iki.fi>
2013-12-03 10:01:55 +0200

    For test purposes, enabled hcp configuration via command line.

--- src/hnetd.c
+++ src/hnetd.c
@@ -25,11 +25,15 @@
 
 #include <libubox/uloop.h>
 
+#include "hcp.h"
 #include "ipc.h"
 #include "platform.h"
 
 int main(__unused int argc, char* const argv[])
 {
+	hcp h;
+	int c;
+
 	if (strstr(argv[0], "hnet-call"))
 		return ipc_client(argv[1]);
 
@@ -37,17 +41,17 @@ int main(__unused int argc, char* const argv[])
 	uloop_init();
 
 	if (getuid() != 0) {
-		syslog(LOG_ERR, "Must be run as root!");
+		L_ERR("Must be run as root!");
 		return 2;
 	}
 
 	if (iface_init()) {
-		syslog(LOG_ERR, "Failed to init platform: %s", strerror(errno));
+		L_ERR("Failed to init platform: %s", strerror(errno));
 		return 3;
 	}
 
 	if (ipc_init()) {
-		syslog(LOG_ERR, "Failed to init IPC: %s", strerror(errno));
+		L_ERR("Failed to init IPC: %s", strerror(errno));
 		return 5;
 	}
 
@@ -57,6 +61,24 @@ int main(__unused int argc, char* const argv[])
 		read(urandom_fd, &seed, sizeof(seed));
 		close(urandom_fd);
 		srandom(seed);
+	}
+
+	h = hcp_create();
+	if (!h) {
+		L_ERR("Unable to initialize HCP");
+		return 42;
+	}
+
+	/* XXX - add real command line parsing at some point. For the
+	 * time being, I've added just this utility to get hcp up and
+	 * running ;) -MSt. */
+	while ((c = getopt(argc, argv, "i:")) != -1) {
+		switch (c) {
+		case 'i':
+			/* internal interface */
+			(void)hcp_set_link_enabled(h, optarg, true);
+			break;
+		}
 	}
 
 	uloop_run();
