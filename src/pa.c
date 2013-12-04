84fe173808b33d626778aa7ce61a5468434e7c46
Oryon <pierre@darou.fr>
2013-12-04 11:38:21 +0100

    Debug: Do not use lazy or for updating

--- src/pa.c
+++ src/pa.c
@@ -1020,9 +1020,9 @@ static void pa_dp_update(struct pa *pa, struct pa_dp *dp,
 {
 	if(!valid_until) {
 		pa_dp_destroy(pa, dp); /* That already tells hcp */
-	} else if(pa_dp_times_set(pa, dp, valid_until, preferred_until) ||
-			pa_dp_dhcpv6_set(pa, dp, dhcpv6_data, dhcpv6_len) ||
-			pa_dp_excluded_set(pa, dp, excluded) ||
+	} else if(pa_dp_times_set(pa, dp, valid_until, preferred_until) |
+			pa_dp_dhcpv6_set(pa, dp, dhcpv6_data, dhcpv6_len) |
+			pa_dp_excluded_set(pa, dp, excluded) |
 			pa_dp_iface_assignbyname(pa, dp, ifname)) {
 		if(dp->local)
 			pa_dp_tell_hcp(pa, dp);
