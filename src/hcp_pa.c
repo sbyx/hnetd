906437060df472bac1ffaba61df7e75a1fcdf761
Markus Stenberg <markus.stenberg@iki.fi>
2013-12-04 13:27:46 +0200

    Added some more commenting + call to pa_flood_subscribe.

--- src/hcp_pa.c
+++ src/hcp_pa.c
@@ -6,8 +6,8 @@
  * Copyright (c) 2013 cisco Systems, Inc.
  *
  * Created:       Wed Dec  4 12:32:50 2013 mstenber
- * Last modified: Wed Dec  4 13:06:26 2013 mstenber
- * Edit time:     12 min
+ * Last modified: Wed Dec  4 13:14:13 2013 mstenber
+ * Edit time:     20 min
  *
  */
 
@@ -21,6 +21,13 @@
 
    _Ideally_ this could actually use external hcp API only. In
    practise, though, I can't be bothered. So I don't.
+
+   Structurally, assigned and delegated prefix handling is
+   different. Delegated prefixes are maintained locally (or well, at
+   least their main key + pointer to TLV is). Assigned prefixes on the
+   other hand are assumed to contain sufficient data within callback
+   arguments to reconstruct the TLV as needed, and therefore there is
+   nothing there here.
 */
 
 #include "hcp_pa.h"
@@ -30,17 +37,23 @@ typedef struct {
   /* HCP notification subscriber structure */
   hcp_subscriber_s subscriber;
 
-
+  /* What are we gluing together anyway? */
+  hcp hcp;
+  pa_t pa;
 } hcp_glue_s, *hcp_glue;
 
 
 static void _tlv_cb(hcp_subscriber s,
                     hcp_node n, struct tlv_attr *tlv, bool add)
 {
+  hcp_glue g = container_of(s, hcp_glue_s, subscriber);
+
 }
 
 static void _node_cb(hcp_subscriber s, hcp_node n, bool add)
 {
+  hcp_glue g = container_of(s, hcp_glue_s, subscriber);
+
 }
 
 static void _updated_lap(const struct prefix *prefix, const char *ifname,
@@ -56,6 +69,7 @@ static void _updated_ldp(const struct prefix *prefix,
                          void *priv)
 {
   hcp_glue g = priv;
+
 }
 
 
@@ -73,12 +87,15 @@ bool hcp_connect_pa(hcp o, pa_t pa)
 
   g->subscriber.tlv_change_callback = _tlv_cb;
   g->subscriber.node_change_callback = _node_cb;
-
+  g->pa = pa;
+  g->hcp = o;
 
   /* Set the rid */
   pa_set_rid(pa, rid);
 
-  /* And let the floodgates open.. ;) */
+  /* And let the floodgates open. pa SHOULD NOT call anything yet, as
+   * it isn't started. hcp, on the other hand, most likely will. */
+  pa_flood_subscribe(pa, &pa_cbs);
   hcp_subscribe(o, &g->subscriber);
 
   return true;
