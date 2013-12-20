8de5fdc3e43182937f7103fe2276da0803010672
Steven Barth <steven@midlink.org>
2013-12-20 12:26:49 +0100

    Add generic platform code and platform abstraction

--- src/ipc.c
+++ src/ipc.c
@@ -3,6 +3,7 @@
 
 #include <sys/un.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 #include <libubox/usock.h>
 #include <libubox/uloop.h>
@@ -11,6 +12,7 @@
 
 #include "ipc.h"
 #include "iface.h"
+#include "dhcpv6.h"
 
 static void ipc_handle(struct uloop_fd *fd, __unused unsigned int events);
 static struct uloop_fd ipcsock = { .cb = ipc_handle };
@@ -20,19 +22,34 @@ enum ipc_option {
 	OPT_COMMAND,
 	OPT_IFNAME,
 	OPT_HANDLE,
+	OPT_PREFIX,
+	OPT_DNS,
 	OPT_MAX
 };
 
 struct blobmsg_policy ipc_policy[] = {
-	[OPT_COMMAND] = {"command", BLOBMSG_TYPE_INT32},
+	[OPT_COMMAND] = {"command", BLOBMSG_TYPE_STRING},
 	[OPT_IFNAME] = {"ifname", BLOBMSG_TYPE_STRING},
 	[OPT_HANDLE] = {"handle", BLOBMSG_TYPE_STRING},
+	[OPT_PREFIX] = {"prefix", BLOBMSG_TYPE_ARRAY},
+	[OPT_DNS] = {"dns", BLOBMSG_TYPE_ARRAY},
 };
 
-enum ipc_command {
-	CMD_IFUP,
-	CMD_IFDOWN,
-	CMD_MAX
+enum ipc_prefix_option {
+	PREFIX_ADDRESS,
+	PREFIX_EXCLUDED,
+	PREFIX_PREFERRED,
+	PREFIX_VALID,
+	PREFIX_CLASS,
+	PREFIX_MAX
+};
+
+struct blobmsg_policy ipc_prefix_policy[] = {
+	[PREFIX_ADDRESS] = {"address", BLOBMSG_TYPE_STRING},
+	[PREFIX_EXCLUDED] = {"excluded", BLOBMSG_TYPE_STRING},
+	[PREFIX_PREFERRED] = {"preferred", BLOBMSG_TYPE_INT32},
+	[PREFIX_VALID] = {"valid", BLOBMSG_TYPE_INT32},
+	[PREFIX_CLASS] = {"class", BLOBMSG_TYPE_INT32}
 };
 
 
@@ -82,7 +99,7 @@ static void ipc_handle(struct uloop_fd *fd, __unused unsigned int events)
 	ssize_t len;
 	struct sockaddr_un sender;
 	socklen_t sender_len = sizeof(sender);
-	struct blob_attr *tb[OPT_MAX];
+	struct blob_attr *tb[OPT_MAX], *p;
 
 	while ((len = recvfrom(fd->fd, buf, sizeof(buf), MSG_DONTWAIT,
 			(struct sockaddr*)&sender, &sender_len)) >= 0) {
@@ -91,13 +108,92 @@ static void ipc_handle(struct uloop_fd *fd, __unused unsigned int events)
 			continue;
 
 		const char *ifname = blobmsg_get_string(tb[OPT_IFNAME]);
+		struct iface *c = iface_get(ifname);
 
-		enum ipc_command cmd = blobmsg_get_u32(tb[OPT_COMMAND]);
-		L_DEBUG("Handling ipc command %d", (int)cmd);
-		if (cmd == CMD_IFUP && tb[OPT_HANDLE]) {
+		const char *cmd = blobmsg_get_string(tb[OPT_COMMAND]);
+		L_DEBUG("Handling ipc command %s", cmd);
+		if (!strcmp(cmd, "ifup") && tb[OPT_HANDLE]) {
 			iface_create(ifname, blobmsg_get_string(tb[OPT_HANDLE]));
-		} else if (cmd == CMD_IFDOWN) {
-			iface_remove(iface_get(ifname));
+		} else if (!strcmp(cmd, "ifdown")) {
+			iface_remove(c);
+		} else if (!strcmp(cmd, "set_v4lease")) {
+			iface_set_v4leased(c, true);
+		} else if (!strcmp(cmd, "unset_v4lease")) {
+			iface_set_v4leased(c, false);
+		} else if (!strcmp(cmd, "set_prefixes") && (p = tb[OPT_PREFIX])) {
+			hnetd_time_t now = hnetd_time();
+			iface_update_delegated(c);
+
+			struct blob_attr *k;
+			unsigned rem;
+			blobmsg_for_each_attr(k, p, rem) {
+				hnetd_time_t valid = HNETD_TIME_MAX, preferred = HNETD_TIME_MAX;
+
+				struct prefix addr = {IN6ADDR_ANY_INIT, 0};
+				struct prefix ex = {IN6ADDR_ANY_INIT, 0};
+				struct blob_attr *tb[PREFIX_MAX];
+				blobmsg_parse(ipc_prefix_policy, PREFIX_MAX, tb,
+						blobmsg_data(k), blobmsg_data_len(k));
+
+				if (!tb[PREFIX_ADDRESS] || !prefix_pton(blobmsg_get_string(tb[PREFIX_ADDRESS]), &addr))
+					continue;
+
+				if (tb[PREFIX_EXCLUDED])
+					prefix_pton(blobmsg_get_string(tb[PREFIX_EXCLUDED]), &ex);
+
+				if (tb[PREFIX_PREFERRED])
+					preferred = now + blobmsg_get_u32(tb[PREFIX_PREFERRED]);
+
+				if (tb[PREFIX_VALID])
+					valid = now + blobmsg_get_u32(tb[PREFIX_VALID]);
+
+				void *data = NULL;
+				size_t len = 0;
+
+#ifdef EXT_PREFIX_CLASS
+				struct dhcpv6_prefix_class pclass = {
+					.type = htons(DHCPV6_OPT_PREFIX_CLASS),
+					.len = htons(2),
+					.class = htons(atoi(blobmsg_get_string(a)))
+				};
+
+				if ((a = tb[PREFIX_CLASS])) {
+					data = &pclass;
+					len = sizeof(pclass);
+				}
+#endif
+				iface_add_delegated(c, &addr, (ex.plen) ? &ex : NULL, valid, preferred, data, len);
+			}
+			iface_commit_delegated(c);
+		} else if (!strcmp(cmd, "set_dhcpv6_data")) {
+			const size_t dns_max = 4;
+			size_t dns_cnt = 0;
+			struct {
+				uint16_t type;
+				uint16_t len;
+				struct in6_addr addr[dns_max];
+			} dns;
+
+			if (tb[OPT_DNS]) {
+				struct blob_attr *k;
+				unsigned rem;
+
+				blobmsg_for_each_attr(k, tb[OPT_DNS], rem) {
+					if (dns_cnt >= dns_max || blobmsg_type(k) != BLOBMSG_TYPE_STRING ||
+							inet_pton(AF_INET6, blobmsg_data(k), &dns.addr[dns_cnt]) < 1)
+						continue;
+
+					++dns_cnt;
+				}
+			}
+
+			if (dns_cnt) {
+				dns.type = htons(DHCPV6_OPT_DNS_SERVERS);
+				dns.len = htons(dns_cnt * sizeof(struct in6_addr));
+				iface_set_dhcpv6_received(c, &dns, ((uint8_t*)&dns.addr[dns_cnt]) - ((uint8_t*)&dns));
+			} else {
+				iface_set_dhcpv6_received(c, NULL, 0);
+			}
 		}
 	}
 }
---
+++ src/platform-generic.c
@@ -0,0 +1,164 @@
+#include <syslog.h>
+#include <errno.h>
+#include <assert.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+
+#include "dhcpv6.h"
+#include "platform.h"
+#include "iface.h"
+#include "prefix_utils.h"
+
+static char backend[] = "/usr/sbin/hnetd-backend";
+
+
+struct platform_iface {
+	pid_t dhcpv4;
+	pid_t dhcpv6;
+};
+
+int platform_init(void)
+{
+	return 0;
+}
+
+// Run platform script
+static pid_t platform_run(char *argv[])
+{
+	pid_t pid = vfork();
+	if (pid == 0) {
+		execv(argv[0], argv);
+		_exit(128);
+	}
+	return pid;
+}
+
+//
+static void platform_call(char *argv[])
+{
+	pid_t pid = platform_run(argv);
+	waitpid(pid, NULL, 0);
+}
+
+// Constructor for openwrt-specific interface part
+void platform_iface_new(struct iface *c, __unused const char *handle)
+{
+	char *argv_dhcpv4[] = {backend, "dhcpv4client", c->ifname, NULL};
+	char *argv_dhcpv6[] = {backend, "dhcpv6client", c->ifname, NULL};
+	assert(c->platform == NULL);
+
+	struct platform_iface *iface = calloc(1, sizeof(*iface));
+	iface->dhcpv4 = platform_run(argv_dhcpv4);
+	iface->dhcpv6 = platform_run(argv_dhcpv6);
+
+	c->platform = iface;
+}
+
+// Destructor for openwrt-specific interface part
+void platform_iface_free(struct iface *c)
+{
+	struct platform_iface *iface = c->platform;
+	if (iface) {
+		kill(iface->dhcpv4, SIGTERM);
+		kill(iface->dhcpv6, SIGTERM);
+
+		free(iface);
+		c->platform = NULL;
+	}
+}
+
+
+void platform_set_internal(struct iface *c, bool internal)
+{
+	char *argv[] = {backend, (internal) ? "setfilter" : "unsetfilter",
+			c->ifname, NULL};
+	platform_call(argv);
+}
+
+
+void platform_set_address(struct iface *c, struct iface_addr *a, bool enable)
+{
+	hnetd_time_t now = hnetd_time();
+	hnetd_time_t valid = (a->valid_until - now) / HNETD_TIME_PER_SECOND;
+	if (valid <= 0)
+		enable = false;
+	else if (valid > UINT32_MAX)
+		valid = UINT32_MAX;
+
+	hnetd_time_t preferred = (a->preferred_until - now) / HNETD_TIME_PER_SECOND;
+	if (preferred < 0)
+		preferred = 0;
+	else if (preferred > UINT32_MAX)
+		preferred = UINT32_MAX;
+
+	char abuf[PREFIX_MAXBUFFLEN], pbuf[10], vbuf[10], cbuf[10] = "";
+	prefix_ntop(abuf, sizeof(abuf), &a->prefix, false);
+	snprintf(pbuf, sizeof(pbuf), "%u", (unsigned)preferred);
+	snprintf(vbuf, sizeof(vbuf), "%u", (unsigned)valid);
+
+	uint8_t *oend = &a->dhcpv6_data[a->dhcpv6_len], *odata;
+	uint16_t olen, otype;
+	dhcpv6_for_each_option(a->dhcpv6_data, oend, otype, olen, odata) {
+#ifdef EXT_PREFIX_CLASS
+		if (otype == DHCPV6_OPT_PREFIX_CLASS && olen == 2) {
+			uint16_t class = (uint16_t)odata[0] << 8 | (uint16_t)odata[1];
+			snprintf(cbuf, sizeof(cbuf), "%u", (unsigned)class);
+		}
+#endif
+	}
+
+	char *argv[] = {backend, (enable) ? "newaddr" : "deladdr",
+			c->ifname, abuf, pbuf, vbuf, cbuf, NULL};
+	platform_call(argv);
+}
+
+
+void platform_set_owner(struct iface *c, bool enable)
+{
+	char *argv[] = {backend, (enable) ? "startdhcp" : "stopdhcp", c->ifname, NULL};
+	platform_call(argv);
+}
+
+
+void platform_set_dhcpv6_send(struct iface *c, const void *dhcpv6_data, size_t len)
+{
+	// DNS options
+	const size_t dns_max = 4;
+	size_t dns_cnt = 0;
+	struct in6_addr dns[dns_max];
+
+	// Add per interface DHCPv6 options
+	uint8_t *oend = ((uint8_t*)dhcpv6_data) + len, *odata;
+	uint16_t olen, otype;
+	dhcpv6_for_each_option(dhcpv6_data, oend, otype, olen, odata) {
+		if (otype == DHCPV6_OPT_DNS_SERVERS) {
+			size_t cnt = olen / sizeof(*dns);
+			if (cnt + dns_cnt > dns_max)
+				cnt = dns_max - dns_cnt;
+
+			memcpy(&dns[dns_cnt], odata, cnt * sizeof(*dns));
+			dns_cnt += cnt;
+		}
+	}
+
+	pid_t pid = fork();
+	if (pid == 0) {
+		char *argv[] = {backend, "setdhcpv6", c->ifname, NULL};
+
+		char *dnsbuf = malloc(dns_cnt * INET6_ADDRSTRLEN + 5);
+		strcpy(dnsbuf, "DNS=");
+		for (size_t i = 0; i < dns_cnt; ++i)
+			inet_ntop(AF_INET6, &dns[i], dnsbuf + strlen(dnsbuf), INET6_ADDRSTRLEN);
+		putenv(dnsbuf);
+
+		execv(argv[0], argv);
+		_exit(128);
+	}
+	waitpid(pid, NULL, 0);
+}
