e6e4780c95d594bd9e2579c2a0773fbfbf996af2
Markus Stenberg <markus.stenberg@iki.fi>
2014-02-13 11:47:42 +0200

    Updated wireshark dissector to work with -00 version of the draft (not all sub-TLVs are decoded, though, oh well).

--- hnetd_wireshark.lua
+++ hnetd_wireshark.lua
@@ -7,8 +7,8 @@
 -- Copyright (c) 2013 cisco Systems, Inc.
 --
 -- Created:       Tue Dec  3 11:13:05 2013 mstenber
--- Last modified: Tue Dec  3 13:42:07 2013 mstenber
--- Edit time:     89 min
+-- Last modified: Thu Feb 13 11:44:59 2014 mstenber
+-- Edit time:     103 min
 --
 
 -- This is Lua module which provides VERY basic dissector for TLVs we
@@ -16,28 +16,28 @@
 
 -- Usage: wireshark -X lua_script:hnetd_wireshark.lua
 
-p_hcp = Proto("hcp", "Homenet Control Protocol")
+p_hncp = Proto("hncp", "Homenet Control Protocol")
 
-local f_id = ProtoField.uint16('hcp.id', 'TLV id')
-local f_len = ProtoField.uint16('hcp.len', 'TLV len')
-local f_data = ProtoField.bytes('hcp.data', 'TLV data', base.HEX)
+local f_id = ProtoField.uint16('hncp.id', 'TLV id')
+local f_len = ProtoField.uint16('hncp.len', 'TLV len')
+local f_data = ProtoField.bytes('hncp.data', 'TLV data', base.HEX)
 
-local f_nid_hash = ProtoField.bytes('hcp.node_identifier_hash', 
+local f_nid_hash = ProtoField.bytes('hncp.node_identifier_hash', 
                                     'Node identifier', base.HEX)
-local f_data_hash = ProtoField.bytes('hcp.data_hash',
+local f_data_hash = ProtoField.bytes('hncp.data_hash',
                                      'Node data hash', base.HEX)
-local f_network_hash = ProtoField.bytes('hcp.network_hash',
+local f_network_hash = ProtoField.bytes('hncp.network_hash',
                                         'Network state hash', base.HEX)
 
-local f_lid = ProtoField.uint32('hcp.llid', 'Local link identifier')
-local f_rlid = ProtoField.uint32('hcp.rlid', 'Remote link identifier')
-local f_upd = ProtoField.uint32('hcp.update_number', 'Update number')
-local f_ms = ProtoField.uint32('hcp.ms_since_origination', 
+local f_lid = ProtoField.uint32('hncp.llid', 'Local link identifier')
+local f_rlid = ProtoField.uint32('hncp.rlid', 'Remote link identifier')
+local f_upd = ProtoField.uint32('hncp.update_number', 'Update number')
+local f_ms = ProtoField.uint32('hncp.ms_since_origination', 
                                'Time since origination (ms)')
 
-p_hcp.fields = {f_id, f_len, f_data,
-                f_nid_hash, f_data_hash, f_network_hash,
-                f_lid, f_rlid, f_upd, f_ms}
+p_hncp.fields = {f_id, f_len, f_data,
+                 f_nid_hash, f_data_hash, f_network_hash,
+                 f_lid, f_rlid, f_upd, f_ms}
 
 local tlvs = {
    [1]={name='link-id', 
@@ -45,33 +45,47 @@ local tlvs = {
                   {4, f_lid}},
    },
    [2]={name='req-net-hash'},
-   [3]={name='req-node-hash',
+   [3]={name='req-node-data',
         contents={{16, f_nid_hash}},
    },
-   [4]={name='node-state',
+
+   [4]={name='network-hash',
+        contents={{16, f_network_hash}}},
+
+   [5]={name='node-state',
         contents={{16, f_nid_hash},
                   {4, f_upd},
                   {4, f_ms},
                   {16, f_data_hash},
         }
    },
-   [5]={name='network-hash',
-        contents={{16, f_network_hash}}},
    [6]={name='node-data', 
         contents={{16, f_nid_hash},
                   {4, f_upd}},
         recurse=true},
-   [7]={name='.. key'},
-   [8]={name='.. neighbor', contents={{16, f_nid_hash},
-                                      {4, f_rlid},
-                                      {4, f_lid},
-                                     }
+   [7]={name='node-data-key'},
+   [8]={name='node-data-neighbor', contents={{16, f_nid_hash},
+                                             {4, f_rlid},
+                                             {4, f_lid},
+                                            },
    },
+   [9]={name='custom'},
+   [41]={name='external-connection', contents={}, recurse=true},
+   [42]={name='delegated-prefix'},
+   [43]={name='assigned-prefix'},
+   [44]={name='dhcp-options'},
+   [45]={name='dhcpv6-options'},
+   [46]={name='router-address'},
+
+   [50]={name='dns-delegated-zone'},
+   [51]={name='dns-router-name'},
+   [52]={name='dns-domain-name'},
+   [60]={name='routing-protocol'},
 }
 
 
-function p_hcp.dissector(buffer, pinfo, tree)
-   pinfo.cols.protocol = 'hcp'
+function p_hncp.dissector(buffer, pinfo, tree)
+   pinfo.cols.protocol = 'hncp'
 
    local rec_decode
    local function data_decode(ofs, left, tlv, tree)
@@ -142,12 +156,17 @@ function p_hcp.dissector(buffer, pinfo, tree)
          end
       end
       -- recursively decode the rest too, hrr :)
+
+      -- (note that we have to round it up to next /4 boundary; stupid
+      -- alignment..)
+      len = math.floor((len + 3)/4) * 4
+
       rec_decode(ofs + len, left - len, tree)
    end
-   rec_decode(0, buffer:len(), tree:add(p_hcp, buffer()))
+   rec_decode(0, buffer:len(), tree:add(p_hncp, buffer()))
 end
 
 -- register as udp dissector
 udp_table = DissectorTable.get("udp.port")
-udp_table:add(8808, p_hcp)
-udp_table:add(38808, p_hcp)
+udp_table:add(8808, p_hncp)
+udp_table:add(38808, p_hncp)
