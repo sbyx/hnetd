a0f1215410901806fbfadde30671bc4c0f5cd911
Markus Stenberg <markus.stenberg@iki.fi>
2014-07-16 21:16:53 +0300

    Made iterations configurable, and BIG by default. Also added correct random seed option, and option to specify particular tests to test only.(Some tests fail at random seeds >100, sigh).

--- run_net_stress.sh
+++ run_net_stress.sh
@@ -8,17 +8,18 @@
 # Copyright (c) 2014 cisco Systems, Inc.
 #
 # Created:       Wed Jul 16 20:26:49 2014 mstenber
-# Last modified: Wed Jul 16 20:28:15 2014 mstenber
-# Edit time:     2 min
+# Last modified: Wed Jul 16 21:12:19 2014 mstenber
+# Edit time:     9 min
 #
 
-# Utility script to invoke the first 100 random seeds 
+ITERATIONS=1000
+
 cmake -DL_LEVEL=1 .
 make test_hncp_net
-for i in `seq 100`
+for i in `seq $ITERATIONS`
 do
     echo -n "."
-    ./test_hncp_net 2>&1 | grep -q SUCCESS || echo $i
+    ./test_hncp_net -r $i $* 2>&1 | grep -q SUCCESS || echo $i
 done
 echo
 cmake -DL_LEVEL=7 .
