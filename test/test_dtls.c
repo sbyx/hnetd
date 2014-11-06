048f969470ce3d3d82c4f58b340c4420f5c7f8fe
Markus Stenberg <markus.stenberg@iki.fi>
2014-11-06 14:58:30 +0200

    Made it possible to run each dtls sub-test separately.

--- test/test_dtls.c
+++ test/test_dtls.c
@@ -6,8 +6,8 @@
  * Copyright (c) 2014 cisco Systems, Inc.
  *
  * Created:       Thu Oct 16 10:57:31 2014 mstenber
- * Last modified: Thu Nov  6 13:11:14 2014 mstenber
- * Edit time:     95 min
+ * Last modified: Thu Nov  6 14:57:59 2014 mstenber
+ * Edit time:     99 min
  *
  */
 
@@ -102,81 +102,74 @@ bool _unknown_cb(dtls d, const char *pem, void *context)
   return true;
 }
 
-static void dtls_basic_2()
+static void _test_basic_i(int i)
 {
-  int i;
-
-  for (i = 0 ; i < 2 ; i++)
-    {
-      int pbase = 49000 + i * 2;
-      dtls d1 = dtls_create(pbase);
-      dtls_set_readable_callback(d1, _readable_cb, NULL);
-      dtls d2 = dtls_create(pbase+1);
-      dtls_set_readable_callback(d2, _readable_cb, NULL);
-      int rv;
-      char *msg = "foo";
-      struct uloop_timeout t = { .cb = _timeout };
-      bool rb;
-      struct sockaddr_in6 src = {.sin6_family = AF_INET6 };
-      struct sockaddr_in6 dst = {.sin6_family = AF_INET6 };
+  int pbase = 49000 + i * 2;
+  dtls d1 = dtls_create(pbase);
+  dtls_set_readable_callback(d1, _readable_cb, NULL);
+  dtls d2 = dtls_create(pbase+1);
+  dtls_set_readable_callback(d2, _readable_cb, NULL);
+  int rv;
+  char *msg = "foo";
+  struct uloop_timeout t = { .cb = _timeout };
+  bool rb;
+  struct sockaddr_in6 src = {.sin6_family = AF_INET6 };
+  struct sockaddr_in6 dst = {.sin6_family = AF_INET6 };
 #ifdef __APPLE__
-      src.sin6_len = sizeof(src);
-      dst.sin6_len = sizeof(dst);
+  src.sin6_len = sizeof(src);
+  dst.sin6_len = sizeof(dst);
 #endif /* __APPLE__ */
-      if (i == 0)
-        {
-          rb = dtls_set_local_cert(d1, "test/cert1.pem", "test/key1.pem");
-          sput_fail_unless(rb, "dtls_set_local_cert 1");
-          rb = dtls_set_verify_locations(d1, "test/cert2.pem", NULL);
-          sput_fail_unless(rb, "dtls_set_verify_locations 1");
+  if (i == 0)
+    {
+      rb = dtls_set_local_cert(d1, "test/cert1.pem", "test/key1.pem");
+      sput_fail_unless(rb, "dtls_set_local_cert 1");
+      rb = dtls_set_verify_locations(d1, "test/cert2.pem", NULL);
+      sput_fail_unless(rb, "dtls_set_verify_locations 1");
 
-          rb = dtls_set_local_cert(d2, "test/cert2.pem", "test/key2.pem");
-          sput_fail_unless(rb, "dtls_set_local_cert 2");
-          rb = dtls_set_verify_locations(d2, "test/cert1.pem", NULL);
-          sput_fail_unless(rb, "dtls_set_verify_locations 2");
-        }
-      else
-        {
-          rb = dtls_set_psk(d1, "foo", 3);
-          sput_fail_unless(rb, "dtls_set_psk");
-
-          rb = dtls_set_psk(d2, "foo", 3);
-          sput_fail_unless(rb, "dtls_set_psk");
-        }
-
-      /* Start the instances once they have been configured */
-      dtls_start(d1);
-      dtls_start(d2);
-
-      /* Send a packet to ourselves */
-      (void)inet_pton(AF_INET6, "::1", &src.sin6_addr);
-      (void)inet_pton(AF_INET6, "::1", &dst.sin6_addr);
-      src.sin6_port = htons(pbase);
-      dst.sin6_port = htons(pbase+1);
-      smock_push_int("dtls_recvfrom", 3);
-      smock_push("dtls_recvfrom_src_in6", &src.sin6_addr);
-      smock_push("dtls_recvfrom_buf", msg);
-      rv = dtls_sendto(d1, msg, strlen(msg), &dst);
-      L_DEBUG("sendto => %d", rv);
-      sput_fail_unless(rv == 3, "sendto failed?");
-      pending_readable = 1;
-
-      uloop_timeout_set(&t, 5000);
-      uloop_run();
-      sput_fail_unless(!pending_readable, "readable left");
-
-      L_DEBUG("killing dtls instances");
-
-      dtls_destroy(d1);
-      dtls_destroy(d2);
-      uloop_timeout_cancel(&t);
-
+      rb = dtls_set_local_cert(d2, "test/cert2.pem", "test/key2.pem");
+      sput_fail_unless(rb, "dtls_set_local_cert 2");
+      rb = dtls_set_verify_locations(d2, "test/cert1.pem", NULL);
+      sput_fail_unless(rb, "dtls_set_verify_locations 2");
     }
+  else
+    {
+      rb = dtls_set_psk(d1, "foo", 3);
+      sput_fail_unless(rb, "dtls_set_psk");
+
+      rb = dtls_set_psk(d2, "foo", 3);
+      sput_fail_unless(rb, "dtls_set_psk");
+    }
+
+  /* Start the instances once they have been configured */
+  dtls_start(d1);
+  dtls_start(d2);
+
+  /* Send a packet to ourselves */
+  (void)inet_pton(AF_INET6, "::1", &src.sin6_addr);
+  (void)inet_pton(AF_INET6, "::1", &dst.sin6_addr);
+  src.sin6_port = htons(pbase);
+  dst.sin6_port = htons(pbase+1);
+  smock_push_int("dtls_recvfrom", 3);
+  smock_push("dtls_recvfrom_src_in6", &src.sin6_addr);
+  smock_push("dtls_recvfrom_buf", msg);
+  rv = dtls_sendto(d1, msg, strlen(msg), &dst);
+  L_DEBUG("sendto => %d", rv);
+  sput_fail_unless(rv == 3, "sendto failed?");
+  pending_readable = 1;
+
+  uloop_timeout_set(&t, 5000);
+  uloop_run();
+  sput_fail_unless(!pending_readable, "readable left");
+
+  L_DEBUG("killing dtls instances");
+
+  dtls_destroy(d1);
+  dtls_destroy(d2);
+  uloop_timeout_cancel(&t);
 }
 
-static void dtls_unknown()
+static void _test_unknown_i(int i)
 {
-  int i;
   char cert1[2048];
   FILE *f;
   char cert2[2048];
@@ -191,70 +184,89 @@ static void dtls_unknown()
   cert2[len] = 0;
   fclose(f);
 
-  for (i = 0 ; i < 2 ; i++)
-    {
-      int pbase = 49100 + i * 2;
-      dtls d1 = dtls_create(pbase);
-      dtls_set_unknown_cert_callback(d1, _unknown_cb, NULL);
-      dtls d2 = dtls_create(pbase+1);
-      dtls_set_unknown_cert_callback(d2, _unknown_cb, NULL);
-      int rv;
-      char *msg = "foo";
-      struct uloop_timeout t = { .cb = _timeout };
-      bool rb;
-      struct sockaddr_in6 src = {.sin6_family = AF_INET6 };
-      struct sockaddr_in6 dst = {.sin6_family = AF_INET6 };
+  int pbase = 49100 + i * 2;
+  dtls d1 = dtls_create(pbase);
+  dtls_set_unknown_cert_callback(d1, _unknown_cb, NULL);
+  dtls d2 = dtls_create(pbase+1);
+  dtls_set_unknown_cert_callback(d2, _unknown_cb, NULL);
+  int rv;
+  char *msg = "foo";
+  struct uloop_timeout t = { .cb = _timeout };
+  bool rb;
+  struct sockaddr_in6 src = {.sin6_family = AF_INET6 };
+  struct sockaddr_in6 dst = {.sin6_family = AF_INET6 };
 #ifdef __APPLE__
-      src.sin6_len = sizeof(src);
-      dst.sin6_len = sizeof(dst);
+  src.sin6_len = sizeof(src);
+  dst.sin6_len = sizeof(dst);
 #endif /* __APPLE__ */
-      rb = dtls_set_local_cert(d1, "test/cert1.pem", "test/key1.pem");
-      sput_fail_unless(rb, "dtls_set_local_cert 1");
+  rb = dtls_set_local_cert(d1, "test/cert1.pem", "test/key1.pem");
+  sput_fail_unless(rb, "dtls_set_local_cert 1");
 
-      if (i == 1)
-        {
-          rb = dtls_set_verify_locations(d1, "test/cert2.pem", NULL);
-          sput_fail_unless(rb, "dtls_set_verify_locations 1");
-        }
-      else
-        smock_push("dtls_unknown_pem", cert2);
-
-      rb = dtls_set_local_cert(d2, "test/cert2.pem", "test/key2.pem");
-      sput_fail_unless(rb, "dtls_set_local_cert 2");
-
-      if (i == 0)
-        {
-          rb = dtls_set_verify_locations(d2, "test/cert1.pem", NULL);
-          sput_fail_unless(rb, "dtls_set_verify_locations 2");
-        }
-      else
-        smock_push("dtls_unknown_pem", cert1);
-
-      /* Start the instances once they have been configured */
-      dtls_start(d1);
-      dtls_start(d2);
-
-      /* Send a packet to ourselves */
-      (void)inet_pton(AF_INET6, "::1", &src.sin6_addr);
-      (void)inet_pton(AF_INET6, "::1", &dst.sin6_addr);
-      src.sin6_port = htons(pbase);
-      dst.sin6_port = htons(pbase+1);
-
-      rv = dtls_sendto(d1, msg, strlen(msg), &dst);
-      L_DEBUG("sendto => %d", rv);
-      sput_fail_unless(rv == 3, "sendto failed?");
-      pending_unknown = 1;
-
-      uloop_timeout_set(&t, 5000);
-      uloop_run();
-
-      dtls_destroy(d1);
-      dtls_destroy(d2);
-      uloop_timeout_cancel(&t);
-
-      sput_fail_unless(!pending_unknown, "no unknown left");
+  if (i == 1)
+    {
+      rb = dtls_set_verify_locations(d1, "test/cert2.pem", NULL);
+      sput_fail_unless(rb, "dtls_set_verify_locations 1");
     }
+  else
+    smock_push("dtls_unknown_pem", cert2);
+
+  rb = dtls_set_local_cert(d2, "test/cert2.pem", "test/key2.pem");
+  sput_fail_unless(rb, "dtls_set_local_cert 2");
+
+  if (i == 0)
+    {
+      rb = dtls_set_verify_locations(d2, "test/cert1.pem", NULL);
+      sput_fail_unless(rb, "dtls_set_verify_locations 2");
+    }
+  else
+    smock_push("dtls_unknown_pem", cert1);
+
+  /* Start the instances once they have been configured */
+  dtls_start(d1);
+  dtls_start(d2);
+
+  /* Send a packet to ourselves */
+  (void)inet_pton(AF_INET6, "::1", &src.sin6_addr);
+  (void)inet_pton(AF_INET6, "::1", &dst.sin6_addr);
+  src.sin6_port = htons(pbase);
+  dst.sin6_port = htons(pbase+1);
+
+  rv = dtls_sendto(d1, msg, strlen(msg), &dst);
+  L_DEBUG("sendto => %d", rv);
+  sput_fail_unless(rv == 3, "sendto failed?");
+  pending_unknown = 1;
+
+  uloop_timeout_set(&t, 5000);
+  uloop_run();
+
+  dtls_destroy(d1);
+  dtls_destroy(d2);
+  uloop_timeout_cancel(&t);
+
+  sput_fail_unless(!pending_unknown, "no unknown left");
 }
+
+static void dtls_basic_1()
+{
+  _test_basic_i(0);
+}
+
+static void dtls_basic_2()
+{
+  _test_basic_i(1);
+}
+
+
+static void dtls_unknown_1()
+{
+  _test_unknown_i(0);
+}
+
+static void dtls_unknown_2()
+{
+  _test_unknown_i(1);
+}
+
 
 int main(int argc, char **argv)
 {
@@ -267,8 +279,10 @@ int main(int argc, char **argv)
   argc -= 1;
   argv += 1;
 
+  sput_maybe_run_test(dtls_basic_1, do {} while(0));
   sput_maybe_run_test(dtls_basic_2, do {} while(0));
-  sput_maybe_run_test(dtls_unknown, do {} while(0));
+  sput_maybe_run_test(dtls_unknown_1, do {} while(0));
+  sput_maybe_run_test(dtls_unknown_2, do {} while(0));
   sput_leave_suite(); /* optional */
   sput_finish_testing();
   return sput_get_return_value();
